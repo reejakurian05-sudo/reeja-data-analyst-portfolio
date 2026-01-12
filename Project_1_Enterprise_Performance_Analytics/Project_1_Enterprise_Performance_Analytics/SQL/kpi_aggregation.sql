b600f83fb281939fc3c6c028d8240d99b383b2c3
Reeja Kurian <reeja.kurian05@gmail.com>
2026-01-12 14:32:54 -0500

    Delete Project_1_Enterprise_Performance_Analytics/Project_1_Enterprise_Performance_Analytics/SQL/kpi_aggregation.sql

--- Project_1_Enterprise_Performance_Analytics/Project_1_Enterprise_Performance_Analytics/SQL/kpi_aggregation.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    BusinessUnit,
-    MetricCategory,
-    COUNT(DISTINCT RecordID) AS TotalRecords,
-    SUM(MetricValue) AS TotalValue
-FROM PerformanceFact
-GROUP BY BusinessUnit, MetricCategory;
