44b20a22527c4575335a447efff8311355b30ae3
Reeja Kurian <reeja.kurian05@gmail.com>
2026-01-12 14:32:17 -0500

    Update data_quality_checks.sql

--- Project_1_Enterprise_Performance_Analytics/SQL/data_quality_checks.sql
+++ Project_1_Enterprise_Performance_Analytics/SQL/data_quality_checks.sql
@@ -1,3 +1,11 @@
+SELECT
+    BusinessUnit,
+    MetricCategory,
+    COUNT(DISTINCT RecordID) AS TotalRecords,
+    SUM(MetricValue) AS TotalValue
+FROM PerformanceFact
+GROUP BY BusinessUnit, MetricCategory;
+
 -- Duplicate record check
 SELECT RecordID, COUNT(*)
 FROM PerformanceFact
